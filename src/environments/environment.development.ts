5239fc4f9d53d32809fbed9a2dbe20c194a2dcd2
James R <jamescrb2012@gmail.com>
2024-07-03 01:46:33 -0500

    update .env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: false,
-    //backendApi: 'https://beexg-api.abexa.pe/gpt',
-    //backendApi: 'http://167.99.54.251:3000/gpt'
-    backendApi: 'http://localhost:3000/gpt',
-    assistantApi: 'http://localhost:3000/sam-assistant'
+    backendApi: 'https://nestgpt.techjrstar.com/gpt',
+    assistantApi: 'https://nestgpt.techjrstar.com/sam-assistant'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: true,
-    //backendApi: 'https://beexg-api.abexa.pe/gpt',
-    //backendApi: 'http://167.99.54.251:3000/gpt'
     backendApi: 'http://localhost:3000/gpt',
     assistantApi: 'http://localhost:3000/sam-assistant'
 };
